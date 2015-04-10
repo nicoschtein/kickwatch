de04880d8e2fdd88560bdd28d1498a6a3d01d1c6
nicoschtein <nicoschtein@gmail.com>
2015-04-10 18:20:22 -0300

    Initial commit of kickwatch.py

---
+++ kickwatch.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+
+from bs4 import BeautifulSoup
+
+import requests
+import time
+import sys
+import webbrowser
+
+class Reward:
+	def __init__(self, name='', desc='', amount=0, backerLimit=-1, backers=0):
+		self.name = name
+		self.desc = desc
+		self.amount = amount
+		self.backerLimit = backerLimit
+		self.backers = backers
+
+	def display(self):
+	   	print("Name : " + self.name +  ", amount: " + self.amount +  ", backers: " + self.backers +  "/" + self.backerLimit + "Desc : " + self.desc + ".")
+
+	def displayShort(self):
+		print(str(self.amount) + '$\t' + self.name  + ' ' + self.backers + '/' + self.backerLimit + ' backers')
+
+	def hasBackerLimit(self):
+		return self.backerLimit > 0
+
+	def isSoldOut(self):
+		return self.backers == self.backerLimit
+
+	def isAvailable(self):
+		return not self.hasBackerLimit() or (self.hasBackerLimit() and not reward.isSoldOut())
+
+def decrement(number):
+	return int(number)-1
+
+def numberize(textToNumberize, toFloat=0):
+	number = textToNumberize.encode('ascii','ignore')
+	nondigits = number.translate(None, '0123456789.')
+	number = number.translate(None, nondigits)
+	if toFloat:
+		return float(number)
+	else:
+		return number
+
+def showRewardsMenu():
+	rewards = []
+	populateRewards(rewards)
+	i = 1
+	for reward in rewards:
+		if reward.hasBackerLimit():
+			print('[' + str(i) + '] ' + str(reward.amount) + '$\t' + reward.name  + ' ' + reward.backers + '/' + reward.backerLimit + ' backers')
+		i += 1
+	return len(rewards)
+
+def checkReward(reward):
+	if reward.hasBackerLimit() and not reward.isSoldOut():
+		print("\n------\nREWARD IS AVAILABLE:\n")
+		print(reward.display())
+		print("\n------\n")
+		webbrowser.open_new_tab(url+"/pledge/new?ref=manage_pledge")
+		time.sleep(10)
+		sys.exit(0)
+	else:
+		print("No luck yet...")
+
+def populateRewards(rewardsArray):
+	
+	print("Fetching data...")
+	r  = requests.get(url)
+
+	data = r.text
+	print("Parsing data...")
+	soup = BeautifulSoup(data)
+
+	rewards_html = soup.find_all('li',class_='NS-projects-reward')
+	getOneRewardInfo = lambda x : x.find_all('div',class_='NS_backer_rewards__reward')[0]
+	rewards_html = map(getOneRewardInfo,rewards_html)
+
+	skip = 0
+	for reward_html in rewards_html:
+		if skip:
+			print("Skipped reward")
+			skip -= 1
+			continue
+
+		reward = Reward()
+			# amount
+		amount = reward_html.h5.contents[0]
+		reward.amount = numberize(amount,1)
+
+		backerLimits_html = reward_html.find_all('p',class_='backers-limits')[0]
+		backersData = backerLimits_html.find('span', class_='num-backers')
+		# backers
+		reward.backers = numberize(backersData.string)
+		# backer limit
+		if backerLimits_html.find('span', class_='sold-out'):
+			reward.backerLimit = reward.backers
+		else:
+			backersLimitData = backerLimits_html.find('span', class_='limited-number')
+			if backersLimitData:
+				reward.backerLimit = numberize(backersLimitData.string.split('left of')[1])	
+		
+		description = reward_html.find_all('div',class_='desc')[0].text.lstrip()
+		# name
+		reward.name = description.split('\n')[0].encode("utf8")
+		# description
+		reward.desc = description.encode("utf8")
+		rewardsArray.append(reward)
+
+interval = raw_input('\nEnter interval for checks in seconds (eg. 60): ')
+interval = int(interval)
+url = raw_input('\nEnter kickstarter campaign url (eg. https://www.kickstarter.com/projects/tiko3d/tiko-the-unibody-3d-printer): ')
+
+rewardCount = showRewardsMenu()
+userRewardsInput = raw_input('Enter one or more reward number separated by spaces (eg. 1 6 8) or none for any limited reward:')
+rewardIndexes = map(decrement, userRewardsInput.split());
+if len(rewardIndexes) == 0:
+	rewardIndexes = xrange(1,rewardCount)
+
+while True:
+	rewards = []
+	populateRewards(rewards)
+	for index in rewardIndexes:
+		reward = rewards[index]
+		print("Reward [" + str(index+1) + "]")
+		print(reward.displayShort())
+		checkReward(reward)
+	time.sleep(interval)
